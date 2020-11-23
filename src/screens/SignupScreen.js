bf786a3ceb66a7b5f6ec01266fe4993afd7c19ff
GeorgeWu <fysidm@gmail.com>
2020-11-23 14:16:24 +0800

    Finish SignupScreen styles

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -5,7 +5,7 @@ import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
@@ -21,10 +21,22 @@ const SignupScreen = ({ navigation }) => {
           title="Sign Up"
         />
       </Spacer>
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  }
+});
 
 export default SignupScreen;
