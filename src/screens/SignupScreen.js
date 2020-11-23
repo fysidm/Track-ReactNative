e7d74b6af6bf63fd1de33f08abe7790df3443a00
GeorgeWu <fysidm@gmail.com>
2020-11-23 14:22:51 +0800

    Setup input props

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
       <Spacer>
@@ -11,10 +14,19 @@ const SignupScreen = ({ navigation }) => {
       </Spacer>
       <Input
         label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
       <Spacer />
       <Input
+        secureTextEntry
         label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
       />
       <Spacer>
         <Button
