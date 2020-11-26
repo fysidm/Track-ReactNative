1eb4e167e4ae00939b313ef2488b513f811e890e
GeorgeWu <fysidm@gmail.com>
2020-11-26 13:20:12 +0800

    Refine a little

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,9 +1,13 @@
-import '../_mockLocation';
+// import '../_mockLocation';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy
+} from 'expo-location';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
