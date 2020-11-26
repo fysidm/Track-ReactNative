a6ca185b09f9b77cb18a7949a25e2bb3d117f330
GeorgeWu <fysidm@gmail.com>
2020-11-26 16:28:02 +0800

    Add axios setting to automatic authentication

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,23 @@
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
-export default axios.create({
+const instance =  axios.create({
   baseURL: 'http://e5db6f92c436.ngrok.io'
 });
+
+instance.interceptors.request.use(
+  // call this function before request send
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  // call this function after request receive error
+  (err) => Promise.reject(err)
+);
+
+export default instance;
