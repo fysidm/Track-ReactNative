01a80b2d7c163bf1832fb7cc14f22ad5fc180399
GeorgeWu <fysidm@gmail.com>
2020-11-26 13:14:55 +0800

    Add mockLocation for testing

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,9 @@
+import '../_mockLocation';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import { requestPermissionsAsync } from 'expo-location';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
@@ -14,6 +15,14 @@ const TrackCreateScreen = () => {
       if (!granted) {
         throw new Error('Location permission not granted');
       }
+
+      await watchPositionAsync({
+        accuracy: Accuracy.BestForNavigation,
+        timeInterval: 1000,
+        distanceInterval: 10
+      }, (location) => {
+        console.log(location);
+      });
     } catch (e) {
       setErr(e);
     }
