55cd62de0579920690b66f8b76ceeb9d63fec7a7
GeorgeWu <fysidm@gmail.com>
2020-11-26 17:12:08 +0800

    Fix some odds

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import { FontAwesome } from '@expo/vector-icons';
 import { Context as AuthContext } from '../context/AuthContext';
 import Spacer from '../components/Spacer';
 
@@ -19,6 +20,11 @@ const AccountScreen = () => {
       </Spacer>
     </SafeAreaView>
   );
+};
+
+AccountScreen.navigationOptions = {
+  title: 'Account',
+  tabBarIcon: <FontAwesome name="gear" size={20} />
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 import { ListItem } from 'react-native-elements';
 import { Context as TrackContext } from '../context/TrackContext';
@@ -10,7 +10,6 @@ const TrackListScreen = ({ navigation }) => {
   return (
     <>
       <NavigationEvents onWillFocus={fetchTracks} />
-      <Text style={{ fontSize: 48 }}>TrackList Screen</Text>
       <FlatList
         data={state}
         keyExtractor={item => item._id}
@@ -29,6 +28,10 @@ const TrackListScreen = ({ navigation }) => {
       />
     </>
   );
+};
+
+TrackListScreen.navigationOptions = {
+  title: 'Tracks'
 };
 
 const styles = StyleSheet.create({});
